3a0284af950666979d9615f4970623f07578c70d
Yusuf Dimari <75980557+yusufdimari@users.noreply.github.com>
2025-03-10 02:42:12 +0000

    Fixed ts erros

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import config from '../config/index.json';
 import Divider from './Divider';
 
 const Footer = () => {
-  const { company, about, contact } = config;
-  const { socialMedia, sections } = about;
+  const { about, contact } = config;
+  const { socialMedia } = about;
 
   return (
     <div
@@ -41,9 +41,9 @@ const Footer = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.2486837758934!2d7.501004399999999!3d9.041065300000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0b937d2a5387%3A0x90dfb15c080ed1!2s1%20Dunukofia%20St%2C%20Garki%2C%20Abuja%20900103%2C%20Federal%20Capital%20Territory%2C%20Nigeria!5e0!3m2!1sen!2suk!4v1741569656900!5m2!1sen!2suk"
               width="600"
               height="450"
-              allowfullscreen
+              // allowfullscreen
               loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
+              // referrerpolicy="no-referrer-when-downgrade"
             />
           </div>
         </div>
